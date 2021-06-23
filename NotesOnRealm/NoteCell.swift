e078a879b7a743fecd46dce63370fd51642d8596
Leonid Shapetko <Magicka@yandex.ru>
2021-06-23 20:37:42 +0300

    Мелкие правки

--- NotesOnRealm/NoteCell.swift
+++ NotesOnRealm/NoteCell.swift
@@ -9,15 +9,14 @@ import UIKit
 
 class NoteCell: UITableViewCell {
 
+    @IBOutlet weak var noteNameLabel: UILabel!
+    @IBOutlet weak var dateOfCreationLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
--- NotesOnRealm/NoteMainPageVC.swift
+++ NotesOnRealm/NoteMainPageVC.swift
@@ -9,81 +9,45 @@ import UIKit
 
 class NoteMainPageVC: UITableViewController {
 
+    var notes: [Note] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.tableView.tableFooterView = UIView()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.reloadNotesTV), name: NSNotification.Name(rawValue: "reloadNotesTV"), object: nil)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        let realmManager = RealmManager()
+        super.viewWillAppear(true)
+        notes = realmManager.takeNotesFromEntity()
+    }
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return notes.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let note = notes[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath) as! NoteCell
+        cell.noteNameLabel.text = note.value(forKey: "noteName") as? String
+        cell.noteLabel.text = note.value(forKey: "note") as? String
+        
+        let date = note.value(forKey: "dateOfCreation") as? Date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        cell.dateOfCreationLabel.text = dateFormatter.string(from: date!)
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    @objc func reloadNotesTV () {
+        let realmManager = RealmManager()
+        notes = realmManager.takeNotesFromEntity()
+        self.tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
